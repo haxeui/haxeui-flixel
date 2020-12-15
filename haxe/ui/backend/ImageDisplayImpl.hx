428d15de5acc10f854f94c5c3131b7ba73c6c424
Ian Harrigan <ianharrigan@hotmail.com>
2020-12-15 08:33:11 +0100

    allow streched images

--- haxe/ui/backend/ImageDisplayImpl.hx
+++ haxe/ui/backend/ImageDisplayImpl.hx
@@ -1,6 +1,5 @@
 package haxe.ui.backend;
 
-import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxImageFrame;
 
 class ImageDisplayImpl extends ImageBase {
@@ -14,9 +13,16 @@ class ImageDisplayImpl extends ImageBase {
             frames = FlxImageFrame.fromFrame(_imageInfo.data);
             
 			aspectRatio = _imageInfo.width / _imageInfo.height;
-			
-			width = frameWidth = _imageInfo.width;
-			height = frameHeight = _imageInfo.height;
+            
+            width = frameWidth = _imageInfo.width;
+            height = frameHeight = _imageInfo.height;
         }
+    }
+    
+    private override function validateDisplay() {
+        var scaleX:Float = _imageWidth / _imageInfo.width;
+        var scaleY:Float = _imageHeight / _imageInfo.height;
+        origin.set(0, 0);
+        scale.set(scaleX, scaleY);
     }
 }
