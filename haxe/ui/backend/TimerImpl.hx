7b9b041717162ee0604de90057d87b351213b538
Ian Harrigan <ianharrigan@hotmail.com>
2021-02-17 18:54:56 +0100

    fix timer

--- haxe/ui/backend/TimerImpl.hx
+++ haxe/ui/backend/TimerImpl.hx
@@ -7,12 +7,15 @@ class TimerImpl {
 	private var _timer:FlxTimer;
 
 	public function new(delay:Int, callback:Void->Void) {
-		
 		_timer = new FlxTimer();
         var d = delay / 1000;
+        var loops = 1;
+        if (d > 0) {
+            loops = 0;
+        }
 		_timer.start(d, function(_) {
 			callback();
-		});
+		}, loops);
 	}
 
 	public function stop() {
