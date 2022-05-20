a3b347dcfa32b963dcdf7329a014b2c8a5901298
Ian Harrigan <ianharrigan@hotmail.com>
2022-05-20 15:27:29 +0200

    flixel should use openfiledialog (from haxeui-openfl)

--- haxe/ui/backend/OpenFileDialogImpl.hx
+++ haxe/ui/backend/OpenFileDialogImpl.hx
@@ -1,26 +1,85 @@
 package haxe.ui.backend;
 
+import haxe.ui.containers.dialogs.Dialogs.SelectedFileInfo;
+#if !js
 import haxe.io.Bytes;
-import haxe.ui.backend.SelectFileDialogBase.SelectedFileInfo;
 import haxe.ui.containers.dialogs.Dialog.DialogButton;
 import openfl.events.Event;
+import openfl.net.FileFilter;
 import openfl.net.FileReference;
 import openfl.net.FileReferenceList;
+#end
 
-class SelectFileDialogImpl extends SelectFileDialogBase {
+using StringTools;
+
+class OpenFileDialogImpl extends OpenFileDialogBase {
+    #if js
+    
+    private var _fileSelector:haxe.ui.util.html5.FileSelector = new haxe.ui.util.html5.FileSelector();
+    
+    public override function show() {
+        var readMode = haxe.ui.util.html5.FileSelector.ReadMode.None;
+        if (options.readContents == true) {
+            if (options.readAsBinary == false) {
+                readMode = haxe.ui.util.html5.FileSelector.ReadMode.Text;
+            } else {
+                readMode = haxe.ui.util.html5.FileSelector.ReadMode.Binary;
+            }
+        }
+        _fileSelector.selectFile(onFileSelected, readMode, options.multiple, options.extensions);
+    }
+    
+    private function onFileSelected(cancelled:Bool, files:Array<SelectedFileInfo>) {
+        if (cancelled == false) {
+            dialogConfirmed(files);
+        } else {
+            dialogCancelled();
+        }
+    }
+    
+    #else
+    
     private var _fr:FileReferenceList = null;
     private var _refToInfo:Map<FileReference, SelectedFileInfo>;
     private var _infos:Array<SelectedFileInfo>;
     
     public override function show() {
-        validateOptions();
-
         _refToInfo = new Map<FileReference, SelectedFileInfo>();
         _infos = [];
         _fr = new FileReferenceList();
         _fr.addEventListener(Event.SELECT, onSelect, false, 0, true);
         _fr.addEventListener(Event.CANCEL, onCancel, false, 0, true);
-        _fr.browse();
+        _fr.browse(buildFileFilters());
+    }
+    
+    private function buildFileFilters():Array<FileFilter> {
+        var f = null;
+        
+        /* DOESNT WORK AS OPENFL DOCS STATE
+        if (options.extensions != null && options.extensions.length > 0) {
+            f = [];
+            for (e in options.extensions) {
+                var ext = e.extension;
+                ext = ext.trim();
+                if (ext.length == 0) {
+                    continue;
+                }
+                var parts = ext.split(",");
+                var finalParts = [];
+                for (p in parts) {
+                    p = p.trim();
+                    if (p.length == 0) {
+                        continue;
+                    }
+                    finalParts.push("*." + p);
+                }
+                
+                f.push(new FileFilter(e.label, finalParts.join(";")));
+            }
+        }
+        */
+        
+        return f;
     }
     
     private function onSelect(e:Event) {
@@ -105,4 +164,6 @@ class SelectFileDialogImpl extends SelectFileDialogBase {
         _fr.removeEventListener(Event.CANCEL, onCancel);
         _fr = null;
     }
+    
+    #end
 }
