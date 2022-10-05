107f520574812d28b14a7e44d073a8bd97f76010
Ian Harrigan <ianharrigan@hotmail.com>
2022-10-05 20:35:34 +0200

    prepare for flixel 5

--- haxe/ui/backend/AppImpl.hx
+++ haxe/ui/backend/AppImpl.hx
@@ -9,7 +9,11 @@ class AppImpl extends AppBase {
 	}
 	
 	private override function build() {
+        #if (flixel < "5.0.0")
 		Lib.current.stage.addChild(new FlxGame(0, 0, FlxHaxeUIAppState, 1, 60, 60, true));
+        #else
+		Lib.current.stage.addChild(new FlxGame(0, 0, FlxHaxeUIAppState, 60, 60, true));
+        #end
         if (Toolkit.backendProperties.getPropBool("haxe.ui.flixel.fps.show")) {
             var x = Toolkit.backendProperties.getPropInt("haxe.ui.flixel.fps.left");
             var y = Toolkit.backendProperties.getPropInt("haxe.ui.flixel.fps.top");
