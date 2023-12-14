26b6bb132c92dfa9b77b4a61eaeda8f9a9efda98
Ian Harrigan <ianharrigan@hotmail.com>
2023-12-14 18:26:32 +0100

    better calllaterimpl (now behaves somewhat like requestAnimationFrame)

--- haxe/ui/backend/CallLaterImpl.hx
+++ haxe/ui/backend/CallLaterImpl.hx
@@ -1,29 +1,33 @@
 package haxe.ui.backend;
 
-class CallLaterImpl {
-    public function new(fn:Void->Void) {
-        haxe.ui.util.Timer.delay(fn, 0);
-    }
-}
-
-/* this version seems to hold the main loop up - unsure why, but animations simply dont work - even though the props seem to be get set correctly
 import flixel.FlxG;
 
+// we'll flip between two list references between updates meaning there is breathing space
+// between calls rather that just filling a list and blocking
 class CallLaterImpl {
+
     private static var added:Bool = false;
-    private static var fns:Array<Void->Void> = [];
+    private static var current:Array<Void->Void>;
+    private static var list1:Array<Void->Void> = [];
+    private static var list2:Array<Void->Void> = [];
     public function new(fn:Void->Void) {
         if (!added) {
-            FlxG.signals.preUpdate.add(onUpdate);
             added = true;
+            current = list1;
+            FlxG.signals.postUpdate.add(onUpdate);
         }
-        fns.insert(0, fn);
+        current.insert(0, fn);
     }
 
     private static function onUpdate() {
-        while (fns.length > 0) {
-            fns.pop()();
+        var ref = current;
+        if (current == list1) {
+            current = list2;
+        } else {
+            current = list1;
+        }
+        while (ref.length > 0) {
+            ref.pop()();
         }
     }
 }
-*/
