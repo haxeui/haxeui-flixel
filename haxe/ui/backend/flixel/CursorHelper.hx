a81c55e806e4a4d2e115097d4ead7e5836dd6425
Sword352 <99792114+Sword352@users.noreply.github.com>
2023-12-23 14:45:43 +0100

    Add a `mouseLoadFunction` callback

--- haxe/ui/backend/flixel/CursorHelper.hx
+++ haxe/ui/backend/flixel/CursorHelper.hx
@@ -4,6 +4,14 @@ class CursorHelper {
     public static var useCustomCursors:Bool = true;
     public static var registeredCursors:Map<String, CursorInfo> = new Map<String, CursorInfo>();
 
+    public static var mouseLoadFunction(default, set):String->BitmapData = (bmd) -> openfl.Assets.getBitmapData(bmd);
+    static function set_mouseLoadFunction(v:String->BitmapData):String->BitmapData {
+        if (v == null)
+            return mouseLoadFunction;
+
+        return mouseLoadFunction = v;
+    }
+
     public static function registerCursor(name:String, graphic:String, scale:Float = 1, offsetX:Int = 0, offsetY:Int = 0) {
         registeredCursors.set(name, {
             name: name, 
