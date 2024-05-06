b9c66194665eeb7e98267479da35411b5d7e29e4
Starmapo <starmapo@proton.me>
2024-05-06 17:12:10 -0400

    Fix resized images not being clipped correctly - `width` and `height` are now set properly for resized image displays - Prevent unnecessary `origin` resets (only needed when the frames change)

--- haxe/ui/backend/ImageDisplayImpl.hx
+++ haxe/ui/backend/ImageDisplayImpl.hx
@@ -1,6 +1,7 @@
 package haxe.ui.backend;
 
 import flixel.graphics.frames.FlxImageFrame;
+import flixel.math.FlxRect;
 import haxe.ui.Toolkit;
 
 class ImageDisplayImpl extends ImageBase {
@@ -15,16 +16,25 @@ class ImageDisplayImpl extends ImageBase {
             frames = FlxImageFrame.fromFrame(_imageInfo.data);
             
             aspectRatio = _imageInfo.width / _imageInfo.height;
-            
-            width = frameWidth = Std.int(_imageInfo.width * Toolkit.scaleX);
-            height = frameHeight = Std.int(_imageInfo.height *  Toolkit.scaleY);
+
+            origin.set(0, 0);
         }
     }
     
     private override function validateDisplay() {
         var scaleX:Float = _imageWidth / (_imageInfo.width / Toolkit.scaleX);
         var scaleY:Float = _imageHeight / (_imageInfo.height / Toolkit.scaleY);
-        origin.set(0, 0);
         scale.set(scaleX, scaleY);
+
+        width = Math.abs(scaleX) * frameWidth;
+        height = Math.abs(scaleY) * frameHeight;
+    }
+
+    override function set_clipRect(rect:FlxRect):FlxRect {
+        if (rect != null) {
+            return super.set_clipRect(FlxRect.get(rect.x / scale.x, rect.y / scale.y, rect.width / scale.x, rect.height / scale.y));
+        } else {
+            return super.set_clipRect(null);
+        }
     }
 }
