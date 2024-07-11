39a78e8a7c053fc90bf0367060756ed7564f2bb4
Starmapo <starmapo@proton.me>
2024-07-11 09:01:43 -0400

    Implement `App.icon` and `Platform.getSystemLocale()`

--- haxe/ui/backend/PlatformImpl.hx
+++ haxe/ui/backend/PlatformImpl.hx
@@ -1,4 +1,9 @@
 package haxe.ui.backend;
 
+import openfl.system.Capabilities;
+
 class PlatformImpl extends PlatformBase {
+    public override function getSystemLocale():String {
+        return Capabilities.language;
+    }
 }
